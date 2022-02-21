bd08b2ecf79eda4968bf5f91d5f461dcbe510feb
Niki <57631867+nikniksham@users.noreply.github.com>
2022-02-21 14:11:49 +0300

    Neeeeeeeeed fixes

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import os
 from flask import Flask, render_template, redirect, request
 from flask_login import LoginManager
 from data.forms import Test, Test2
 from data.user import User
 from user.user import user
 from session import db_session
+import random
 import config
-from PIL import Image
+import os
+
+let = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+ALLOWED_EXTENSIONS = ['pdf', 'png', 'jpg', 'jpeg', 'svg']
 
 app = Flask(__name__)
 app.config.from_object(config)
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+def create_random_name(name_len):
+    return ''.join([random.choice(let) for i in range(name_len)])
 
 
 def delete_img(filename):
@@ -20,11 +27,27 @@ def delete_img(filename):
             os.remove(f"{app.config['UPLOAD_FOLDER']}{filename}.{image_format}")
 
 
-def get_files_from(folder, path=app.config["UPLOAD_FOLDER"]):
+def clear_folder(folder_name, path=app.config['UPLOAD_FOLDER']):
+    delete_folder(folder_name, path=path)
+    os.makedirs(path+folder_name)
+
+
+def delete_folder(folder_name, path=app.config['UPLOAD_FOLDER']):
+    if os.path.exists(path+folder_name):
+        for filename in os.listdir(path + folder_name):
+            if os.path.isdir(f"{path}{folder_name}/{filename}"):
+                delete_folder(f"{folder_name}/{filename}")
+            else:
+                os.remove(f"{path}{folder_name}/{filename}")
+        os.rmdir(path+folder_name)
+
+
+def get_files_from(folder, path=app.config["UPLOAD_FOLDER"], add_path=None):
     files = []
     if os.path.exists(path+folder):
         files = os.listdir(path+folder)
-    files = [folder+"/"+f for f in files]
+    files = [(folder+"/"+f) if not add_path else ("/"+add_path+folder+"/"+f) for f in files]
+    print(files)
     return files
 
 
@@ -57,26 +80,12 @@ def test():
     error = "Error"
     upload_folder = "for_test"
     if request.method == "POST":
-        print(request.files)
-    """
-    upload_folder = "for_test"
-    if request.method == "POST":
-        for i in range(1, 21):
-            inf = form[f"image_{i}"].data
-            # print(form[f"image_{i}"])
-            if inf.filename != "":
-                if inf.filename.split(".")[-1] in app.config['SUPPORTED_FORMATS']:
-                    print(form[f"image_{i}"])
-                    print(type(form[f"image_{i}"]))
-                    delete_img(f"{upload_folder}/{inf.name}")
-                    inf.save(f'{app.config["UPLOAD_FOLDER"]}{upload_folder}/{inf.name}.{inf.filename.split(".")[-1]}')
-            else:
-                delete_img(f"{upload_folder}/{inf.name}")
-    filenames = {}
-    for i, filename in enumerate(get_files_from("for_test"), start=1):
-        filenames[f"image_{i}"] = "/" + app.config["UPLOAD_FOLDER"] + filename
-    """
-    return render_template("test.html", form=form, error=error, form_title="Test page", filenames=["/static/img/img.png"])
+        files = request.files
+        clear_folder("for_test")
+        for ind, key in enumerate(dict(files).keys()):
+            if files[key].filename != "" and files[key].filename.split(".")[-1] in ALLOWED_EXTENSIONS:
+                files[key].save(f'{app.config["UPLOAD_FOLDER"]}{upload_folder}/{ind}{create_random_name(50)}.{files[key].filename.split(".")[-1]}')
+    return render_template("test.html", form=form, error=error, form_title="Test page", filenames=get_files_from("for_test", add_path=app.config["UPLOAD_FOLDER"]))
 
 
 @app.route("/test2/", methods=['GET', 'POST'])
